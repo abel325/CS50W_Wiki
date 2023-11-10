fd57fe42dc323ee73d8626d0fa03d311d6571a0b
Abel Mehedinteanu <me.abel52@gmail.com>
2023-11-10 09:15:49 +0200

    Make Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import markdown2
 
 from . import util
 
@@ -8,3 +11,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entries(request, name):
+    entry = util.get_entry(name)
+    if entry != None:
+        htmlEntry = markdown2.markdown(entry)
+        return render(request, "encyclopedia/entries.html", {
+            "title": name,
+            "entry": htmlEntry
+        })
+    else:
+        return render(request, "encyclopedia/entries.html", {
+            "title": "Error 404",
+            "entry": "<h1 style='text-align: center;'> Error 404: file not found </h1>"
+        })
